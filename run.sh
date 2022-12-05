f829031a1ccae4b98d505597e30d20bff224428d
Sven-Thorsten Dietrich <sxd1425@miani.edu>
2022-12-05 00:29:32 -0500

    Add run script; fix fam reference, move control files into subdirs

---
+++ run.sh
@@ -0,0 +1,4 @@
+# !/bin/bash
+
+docker run -v /hihg:/hihg -v /Volumes/Synology:/Volumes/Synology -v /home/sven/checkout/hihg_um/qc_vcf:/app  -it hihg-um/sven/perl make_20K_multi_x_flagged_thread_vcf.pl $@
+
