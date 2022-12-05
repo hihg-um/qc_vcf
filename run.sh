b87b9e91bdc9d22c8ac431a1a141a7fa2de4adc2
Sven-Thorsten Dietrich <sxd1425@miani.edu>
2022-12-05 14:40:00 -0500

    Add time for docker completion

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 
-docker run -v /hihg:/hihg -v /Volumes/Synology:/Volumes/Synology -v /home/sven/checkout/hihg_um/qc_vcf:/app  -it hihg-um/sven/perl scripts/make_20K_multi_x_flagged_thread_vcf.pl $@
+time docker run -v /hihg:/hihg -v /Volumes/Synology:/Volumes/Synology -v /home/sven/checkout/hihg_um/qc_vcf:/app  -it hihg-um/sven/perl scripts/make_20K_multi_x_flagged_thread_vcf.pl $@
 
